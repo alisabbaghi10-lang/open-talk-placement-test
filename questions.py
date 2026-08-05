df9174ec9fcc403be341bc0fd7a73dd879fa3637
alisabbaghi10-lang <alisabbaghi10@gmail.com>
2026-08-05 13:55:07 +0330

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,1 +1,46 @@
+QUESTIONS = [
 
+    {
+        "id": 1,
+        "type": "grammar",
+        "question": "I ___ from Iran.",
+        "options": ["am", "is", "are", "be"],
+        "answer": 0
+    },
+
+    {
+        "id": 2,
+        "type": "grammar",
+        "question": "She ___ coffee every morning.",
+        "options": ["drink", "drinks", "drinking", "drank"],
+        "answer": 1
+    },
+
+    {
+        "id": 3,
+        "type": "grammar",
+        "question": "They ___ TV now.",
+        "options": ["watch", "watched", "are watching", "watching"],
+        "answer": 2
+    },
+
+    {
+        "id": 4,
+        "type": "grammar",
+        "question": "We ___ to the park yesterday.",
+        "options": ["go", "went", "gone", "going"],
+        "answer": 1
+    },
+
+    {
+        "id": 5,
+        "type": "grammar",
+        "question": "I ___ my homework yet.",
+        "options": [
+            "didn't finish",
+            "haven't finished",
+            "don't finish",
+            "won't finish"
+        ],
+        "answer": 1
+    },
